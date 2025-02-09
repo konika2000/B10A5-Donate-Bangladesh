de12d9566d517fce0cd9e40b6bce2c2c92544232
konika2000 <konikaakter.cse@gmail.com>
2025-02-10 00:42:33 +0600

    Basic html project

---
+++ js/donation.js
@@ -0,0 +1,5 @@
+document.getElementById('btn-donation').addEventListener('click', function(event){
+event.preventDefault();
+const donateForNoakhali = getInputFieldValueById('input-donation');
+console.log("pooki")
+});
