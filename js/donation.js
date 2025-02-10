7183b07e764b955f40993a60b507dba98d367e50
konika2000 <konikaakter.cse@gmail.com>
2025-02-10 23:41:38 +0600

    donate for Noakhali section done

--- js/donation.js
+++ js/donation.js
@@ -1,5 +1,17 @@
-document.getElementById('btn-donation').addEventListener('click', function(event){
+document.getElementById('btn-donate-now').addEventListener('click', function(event){
 event.preventDefault();
 const donateForNoakhali = getInputFieldValueById('input-donation');
-console.log("pooki")
+const accountBalance=getTextFieldValueById("account-balance");
+if(typeof donateForNoakhali === "number" && !isNaN(donateForNoakhali) && (donateForNoakhali >0) && (donateForNoakhali <= accountBalance)){
+   const nBalance = getTextFieldValueById('noakhali-acc-blnc');
+   const NnewBalance = nBalance + donateForNoakhali;
+   document.getElementById('noakhali-acc-blnc').innerText = NnewBalance;
+
+  
+  const newBalance =accountBalance - donateForNoakhali;
+  document.getElementById('account-balance').innerText = newBalance;
+}
+else{
+    alert('Wrong Input')
+}
 });
--- js/utility.js
+++ js/utility.js
@@ -1,5 +1,11 @@
 function getInputFieldValueById(id){
     const inputValue = document.getElementById(id).value ;
-    const inputNumber = parseFloat(inputValue)
+    const inputNumber = parseFloat(inputValue);
     return inputNumber;
+}
+
+function getTextFieldValueById(id){
+    const textValue = document.getElementById(id).innerText;
+    const textNumber = parseFloat(textValue);
+    return textNumber;
 }
